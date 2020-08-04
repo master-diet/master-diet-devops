ff04d61a66871e4f72b55cefabc9bbb2526884e8
jakubowiczish <jakub77plotnikowski@gmail.com>
2020-08-04 23:54:42 +0200

    Add dev_scripts directory with all scripts that might be useful for development deploy

---
+++ prepare_dev.sh
@@ -0,0 +1,5 @@
+mkdir master-diet
+mv -a /dev_scripts/. /master-diet/
+cd master-diet
+git clone https://github.com/master-diet/master-diet-backend
+git clone https://github.com/master-diet/master-diet-frontend
