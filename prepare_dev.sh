4eb632b35c161c08ae94e7a338e79ec1f351f2fb
jakubowiczish <jakub77plotnikowski@gmail.com>
2020-08-05 00:02:46 +0200

    Update prepare_dev.sh

--- prepare_dev.sh
+++ prepare_dev.sh
@@ -1,5 +1,6 @@
 mkdir master-diet
-mv /dev_scripts/* /master-diet/
+mv dev_scripts/* /master-diet/
 cd master-diet
 git clone https://github.com/master-diet/master-diet-backend
 git clone https://github.com/master-diet/master-diet-frontend
+chmod -R +rx .
