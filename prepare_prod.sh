5ac940b2c0c57983e17319fcc03567f8e8288962
jakubowiczish <jakub77plotnikowski@gmail.com>
2020-08-05 22:14:39 +0200

    Add basis of prod scripts

---
+++ prepare_prod.sh
@@ -0,0 +1,7 @@
+mkdir master-diet
+mv prod_scripts/* master-diet/
+cd master-diet
+git clone https://github.com/master-diet/master-diet-secrets
+git clone https://github.com/master-diet/master-diet-backend
+git clone https://github.com/master-diet/master-diet-frontend
+chmod -R +rx .
---
+++ prod_scripts/run_db.sh
@@ -0,0 +1,1 @@
+docker-compose -f docker-compose-prod.yml up --build master-diet-db
