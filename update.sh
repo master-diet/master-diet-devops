a3082a66feb8ce7263c5ac00f5a441c50328ee3c
jakubowiczish <jakub77plotnikowski@gmail.com>
2020-08-11 22:04:57 +0200

    Add update.sh for automatic pulling all repositories

---
+++ update.sh
@@ -0,0 +1,5 @@
+git pull origin master
+cd master-diet
+cd master-diet-backend && git pull origin master && cd ..
+cd master-diet-frontend && git pull origin master && cd ..
+cd master-diet-secrets && git pull origin master && cd ..
